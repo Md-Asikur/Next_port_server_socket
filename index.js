543fd73bb759e6945bd6e04c30c116b53d269325
Md-Asikur <asikurrahaman997@gamil.com>
2023-04-01 07:07:33 -0700

    v35

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
